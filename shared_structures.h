c9f052e9b5d2e76d21a55adcd0152a8e179b40a6
Александр Новожилов <ivningman@gmail.com>
2022-03-21 03:33:45 +0300

    new version

--- shared_structures.h
+++ shared_structures.h
@@ -19,6 +19,10 @@ public:
 		}
 	}
 
+	static fixed_array<T>& from_struct_pointer(void* ptr) {
+		return *((fixed_array<T>*)ptr);
+	}
+
 	inline std::size_t get_max_size() {
 		return _size;
 	}
@@ -30,63 +34,24 @@ public:
 	inline T& operator[](std::size_t pos) {
 		return *(T*)((std::size_t)this + sizeof(fixed_array) + pos * sizeof(T));
 	}
-	
-	static void* heap_builder_allocate(std::size_t size) {
-		fixed_array<T>* result = (fixed_array<T>*)malloc(size * sizeof(T) + sizeof(fixed_array<T>));
+
+	static std::pair<std::size_t, fixed_array<T>*> build(std::size_t size) {
+		std::size_t bytes_to_allocate = size * sizeof(T) + sizeof(fixed_array<T>);
+		fixed_array<T>* result = (fixed_array<T>*)malloc(bytes_to_allocate);
 		new(result) fixed_array<T>(size);
-		return (void*)result;
+		return { bytes_to_allocate, result };
 	}
 
-};
-
-template<typename T>
-struct fixed_stack{
-private:
-	std::size_t _top;
-	fixed_array<T> _container;
-public:
-	fixed_stack(std::size_t size) : _container(fixed_array<T>(size)) {
-		_top = 0;
+	void destroy_after_build() {
+		free((void*)this);
 	}
 
-	void push(const T& what) {
-		if (_top + 1 > _container.get_max_size()) {
-			throw std::runtime_error("Can't push: stack overflow");
-		}
-		_container[_top++] = what;
+	static void copy_to(void* from, void* to) {
+		auto a = (fixed_array<T>*) from;
+		auto b = (fixed_array<T>*) to;
+		memcpy(to, from, a->get_size());
 	}
 
-	T& top() {
-		if (_top < 1) {
-			throw std::runtime_error("Can't top from empty stack");
-		}
-		return _container[_top - 1];
-	}
-
-	void pop() {
-		if (_top < 1) {
-			throw std::runtime_error("Can't pop from empty stack");
-		}
-		--_top;
-	}
-
-	std::size_t size() {
-		return _top;
-	}
-
-	bool empty() {
-		return _top == 0;
-	}
-
-	std::size_t get_size() {
-		return _container.get_size() + sizeof(fixed_stack<T>) - sizeof(fixed_array<T>);
-	}
-
-	static void* heap_builder_allocate(std::size_t size) {
-		fixed_stack<T>* result = (fixed_stack<T>*)malloc(size * sizeof(T) + sizeof(fixed_stack<T>));
-		new(result) fixed_stack<T>(size);
-		return (void*)result;
-	}
 };
 
 typedef struct struct_wrapper struct_wrapper_;
@@ -94,10 +59,14 @@ typedef struct struct_wrapper struct_wrapper_;
 struct struct_builder {
 private:
 	bool is_done;
-	std::vector<std::pair<std::size_t, void*>> fields;
+	void* data;
+	std::size_t current_size;
+	std::vector<std::pair<std::size_t, void(*)(void*, void*)>> fields;
 public:
 	struct_builder() {
 		is_done = 0;
+		current_size = 0;
+		data = 0;
 	}
 
 
@@ -106,10 +75,28 @@ public:
 		if (is_done) {
 			throw std::runtime_error("Structure build is done, can't add other structures.");
 		}
-		T* obj = (T*) T::heap_builder_allocate(args...);
-		fields.push_back({ obj->get_size(), obj });
-		return *this;
-	}
+
+		// Build structure
+		std::pair<std::size_t, T*> obj = T::build(args...);
+
+		// Allocate more data
+		if (data) {
+			data = realloc(data, current_size + obj.first);
+		}
+		else {
+			data = malloc(obj.first);
+		}
+
+		// Copy to allocated raw data
+		T::copy_to((void*)obj.second, (void*)((size_t)data + current_size));
+
+		// Increment size
+		current_size += obj.first;
+		fields.push_back({ obj.first, static_cast<void(*)(void*,void*)>(&(T::copy_to)) });
+
+		// Destroy original object
+		obj.second->destroy_after_build();
+		return *this;	}
 
 	struct_wrapper_& build(void* (*allocator)(std::size_t));
 
@@ -117,77 +104,150 @@ public:
 };
 
 struct struct_wrapper {
+
 	std::size_t offsets_offset;
 	std::size_t offsets_count;
 
 	std::size_t get_size() {
-		return sizeof(struct_wrapper) + offsets_offset + offsets_count * sizeof(std::size_t);
+		return offsets_offset + offsets_count * sizeof(std::size_t);
 	}
 
 	template<typename T>
 	T* get_at_offset(std::size_t offset) {
-		return (T*)((std::size_t)this + sizeof(struct_wrapper) + offset);
+		return (T*)((std::size_t)this + offset);
 	}
 
 	inline std::size_t& get_offset(std::size_t index) {
 		return *get_at_offset<std::size_t>(offsets_offset + index * sizeof(std::size_t));
 	}
 
-	template<typename T>
-	T& get_struct(std::size_t idx) {
+	void* get_struct_ptr(std::size_t idx) {
 		if (idx >= offsets_count) {
 			throw std::runtime_error("Can't get struct at idx=" + std::to_string(idx) + "(offsets_count=" + std::to_string(offsets_count) + ").");
 		}
-		return *get_at_offset<T>(get_offset(idx));
+		return get_at_offset<void>(get_offset(idx));
 	}
 
 };
 
+#include <iostream>
+
 struct_wrapper& struct_builder::build(void* (*allocator)(std::size_t)) {
-	std::size_t summary_size = sizeof(struct_wrapper) + sizeof(std::size_t) * fields.size();
-	for (auto& field : fields) {
-		summary_size += field.first;
-	}
+	std::size_t summary_size = sizeof(struct_wrapper) + sizeof(std::size_t) * fields.size() + current_size;
 
 	struct_wrapper* sw = (struct_wrapper*)allocator(summary_size);
 
-	std::size_t current_offset = 0;
-	for (auto& field : fields) {
-		memcpy(sw->get_at_offset<void>(current_offset), field.second, field.first);
-		current_offset += field.first;
-		free(field.second);
-	}
-
-	sw->offsets_offset = current_offset;
+	sw->offsets_offset = sizeof(struct_wrapper) + current_size;
 	sw->offsets_count = fields.size();
-	current_offset = 0;
-	std::size_t index = 0;
 
+	std::size_t current_offset = 0;
+	std::size_t index = 0;
 	for (auto& field : fields) {
-		sw->get_offset(index++) = current_offset;
+		sw->get_offset(index++) = current_offset + sizeof(struct_wrapper);
+		field.second((void*)((size_t)data + current_offset), (void*)((size_t)sw + current_offset + sizeof(struct_wrapper)));
 		current_offset += field.first;
 	}
 
+	free(data);
 	return *sw;
 }
 
 struct shared_struct {
 protected:
 	struct_wrapper& wrapper;
+	std::size_t counter;
 public:
-	shared_struct(struct_wrapper& wrap) : wrapper(wrap) {
 
-	}
-	void* get_struct_address() {
-		return &wrapper;
-	}
-
-	std::size_t get_struct_size() {
+	std::size_t get_size() {
 		return wrapper.get_size();
 	}
+
+	shared_struct(struct_wrapper& wrap) : wrapper(wrap), counter(0){
+
+	}
+
 };
 
-#define init_field(field) field(wrapper.get_struct<std::remove_reference<decltype(field)>::type>(counter++))
-#define add_field(field, ...) add<std::remove_reference<decltype(field)>::type>(__VA_ARGS__)
+#define get_type(field) std::remove_reference<decltype(field)>::type
+#define init_field(field) field(get_type(field)::from_struct_pointer(wrapper.get_struct_ptr(counter++)))
+#define add_field(field, ...) .add<std::remove_reference<decltype(field)>::type>(__VA_ARGS__)
+
+#define generate_constructor(class_name, ...) class_name (void* ptr = nullptr) : shared_struct(build_by_ptr(ptr, shared_allocator)), init_vars(__VA_ARGS__) {}
+
+#define generate_methods(class_name) \
+static class_name& from_struct_pointer(void* x) {\
+	auto res = (new class_name(x));\
+	return *res;\
+}\
+static std::pair<std::size_t, class_name*> build() {\
+	auto& res = build_by_ptr(nullptr, malloc);\
+	return { res.get_size(), (class_name*)&res };\
+}\
+static void copy_to(void* from, void* to) {\
+	auto a = (struct_wrapper*)from;\
+	auto b = (struct_wrapper*)to;\
+	memcpy(to, from, a->get_size());\
+}\
+\
+void destroy_after_build() {\
+	free((void*)this);\
+}
+
+#define builder_start static struct_wrapper& build_by_ptr(void* ptr, void* (*allocator)(std::size_t)) {\
+if (!ptr) {\
+	ptr = (void*)&struct_builder()
+
+#define builder_end .build(allocator);\
+}\
+return *((struct_wrapper*)ptr);\
+}
+
+
+#define CHECK_N(x, n, ...) n
+#define CHECK(...) CHECK_N(__VA_ARGS__, 0,)
+#define PROBE(x) x, 1,
+#define IS_PAREN(x) CHECK(IS_PAREN_PROBE x)
+#define IS_PAREN_PROBE(...) PROBE(~)
+#define COMPL(b) PRIMITIVE_CAT(COMPL_, b)
+#define COMPL_0 1
+#define COMPL_1 0
+#define EMPTY()
+#define DEFER(id) id EMPTY()
+#define OBSTRUCT(...) __VA_ARGS__ DEFER(EMPTY)()
+#define NOT(x) CHECK(PRIMITIVE_CAT(NOT_, x))
+#define NOT_0 PROBE(~)
+#define BOOL(x) COMPL(NOT(x))
+#define IF(c) IIF(BOOL(c))
+
+#define EAT(...)
+#define EXPAND(...) __VA_ARGS__
+#define EVAL2(...) EVAL3(EVAL3(EVAL3(__VA_ARGS__)))
+#define EVAL3(...) EVAL4(EVAL4(EVAL4(__VA_ARGS__)))
+#define EVAL4(...) EVAL5(EVAL5(EVAL5(__VA_ARGS__)))
+#define EVAL5(...) __VA_ARGS__
+#define oo(x) int(x)
+
+#define WHILE(pred, fst, ...) \
+    IF(pred(fst)) \
+    ( \
+		OBSTRUCT(WHILE_INDIRECT) () \
+        ( \
+            pred, __VA_ARGS__, init_field(fst) \
+        ),  \
+         __VA_ARGS__\
+    )
+
+#define IIF(c) PRIMITIVE_CAT(IIF_, c)
+#define IIF_0(t, ...) __VA_ARGS__
+#define IIF_1(t, ...) t
+
+#define CAT(a, ...) PRIMITIVE_CAT(a, __VA_ARGS__)
+#define PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
+
+#define WHILE_INDIRECT() WHILE
+#define MM(x, ...) __VA_ARGS__
+#define MX(x, ...) x, 1, 
+#define WWW(f, ...) NOT(IS_PAREN(f))
+#define init_vars(...) EVAL2(WHILE(WWW, __VA_ARGS__, ()))
 
 #endif
